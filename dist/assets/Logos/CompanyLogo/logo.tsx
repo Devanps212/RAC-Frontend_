cc03dc0033e2bb029c88295968a4836f4129cdcc
Devanps <devanps212@gmail.com>
2024-06-24 16:00:19 +0530

    some bug fix

---
+++ dist/assets/Logos/CompanyLogo/logo.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "react-bootstrap"
+import React from "react" 
+
+const CompanyLogo = () =>{
+    return(
+        <Navbar.Brand>
+            <img
+            src="./2a-b090-63d31c75e6a6-qcoku1709465106.svg"
+            height='30px'
+            width='25px'
+            alt="Logo"
+            />
+        </Navbar.Brand>
+    )
+}
+
+export default CompanyLogo
