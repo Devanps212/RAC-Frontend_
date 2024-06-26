f8f0e839473445229fb1f082457fe6471ec6b877
Devanps <devanps212@gmail.com>
2024-06-26 13:46:42 +0530

    sample big fix

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -6,7 +6,8 @@ interface ImportMetaEnv {
     readonly VITE_BACKEND_SERVER: string,
     readonly VITE_SCOKET: string,
     readonly VITE_ENVIRONMENT: string,
-    readonly VITE_STRIPE_KEY: string
+    readonly VITE_STRIPE_KEY: string,
+    readonly VITE_BACKEND_SERVER_LOCAL: string,
   }
   
   interface ImportMeta {
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const configKeys  = {
-    API_URL:import.meta.env.VITE_BACKEND_SERVER,
+    API_URL:import.meta.env.VITE_ENVIRONMENT === "production" ? import.meta.env.VITE_BACKEND_SERVER : import.meta.env.VITE_BACKEND_SERVER_LOCAL,
     SOCKET_PORT: import.meta.env.VITE_SCOKET
 }
 export default configKeys
