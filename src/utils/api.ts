8d1ac7cdcc0705ec957c2a1a4329b7b4b14868b6
Devanps <devanps212@gmail.com>
2024-06-26 14:09:22 +0530

    bug fix sample

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const configKeys  = {
-    API_URL:import.meta.env.VITE_BACKEND_SERVER,
+    API_URL:import.meta.env.VITE_BACKEND_SERVER, //change to local host to run in local machine port 5000
     SOCKET_PORT: import.meta.env.VITE_SCOKET
 }
 export default configKeys
