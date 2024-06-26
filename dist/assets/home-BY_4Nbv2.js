55bdb9aa65d1e056347d788487d3f06ce8368139
Devanps <devanps212@gmail.com>
2024-06-26 13:59:52 +0530

    bug fix 3

---
+++ dist/assets/home-BY_4Nbv2.js
@@ -0,0 +1,1 @@
+import{j as r,b1 as s}from"./index-D8EY0t2m.js";const e=()=>r.jsx("h1",{children:"Dashboard"}),t=()=>r.jsx(s,{children:r.jsx(e,{})});export{t as default};
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const configKeys  = {
-    API_URL:import.meta.env.VITE_ENVIRONMENT === "dev" ? import.meta.env.VITE_BACKEND_SERVER_LOCAL : import.meta.env.VITE_BACKEND_SERVER,
+    API_URL:import.meta.env.VITE_BACKEND_SERVER,
     SOCKET_PORT: import.meta.env.VITE_SCOKET
 }
 export default configKeys
