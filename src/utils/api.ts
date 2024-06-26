49009c412cbd3be9b629e6a17dbc209fea0083f3
Devanps <devanps212@gmail.com>
2024-06-26 13:57:08 +0530

    sample bug fix 2

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const configKeys  = {
-    API_URL:import.meta.env.VITE_ENVIRONMENT === "production" ? import.meta.env.VITE_BACKEND_SERVER : import.meta.env.VITE_BACKEND_SERVER_LOCAL,
+    API_URL:import.meta.env.VITE_ENVIRONMENT === "dev" ? import.meta.env.VITE_BACKEND_SERVER_LOCAL : import.meta.env.VITE_BACKEND_SERVER,
     SOCKET_PORT: import.meta.env.VITE_SCOKET
 }
 export default configKeys
